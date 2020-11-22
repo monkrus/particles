1d099216c320962935952dad7f04f86c2fb6e8f7
Sergei Stadnik <sergeisqa@gmail.com>
2020-11-22 08:24:12 -0700

    upd

--- main.go
+++ main.go
@@ -7,16 +7,30 @@ import (
 )
 
 func main() {
-	 input :=  
-     
 
+	// make an empty slice
+	input := make([]string, 0)
+
+	//create new scanner
 	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 
+	//
+	for {
+		fmt.Print("Numbers: ")
+
+		scanner.Scan()
+
+		text := scanner.Text()
+
+		if len(text) != 0 {
+
+			fmt.Println(text)
+
+			input = append(input, text)
+		} else {
+			break
+		}
 	}
 
+	fmt.Println(input)
 }
