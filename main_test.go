b5b151dee35edd27007ccbb7c00cf512ba8901aa
cyberS-El <74835012+cyberS-El@users.noreply.github.com>
2020-11-30 23:20:49 +0200

    Update

--- main_test.go
+++ main_test.go
@@ -1,6 +1,27 @@
 package main
 
 import (
-  "testing"
+	"fmt"
+	"strings"
+	"testing"
 )
 
+func TestConstruct(t *testing.T) {
+	var tests = []struct {
+		caps     map[int]int
+		tables   map[int]struct{}
+		n        int
+		expected string
+	}{
+		{map[int]int{}, map[int]struct{}{}, 6, strings.Repeat(" ", 6)},
+		{map[int]int{0: 1, 2: 3}, map[int]struct{}{1: struct{}{}}, 6, ".T:.  "},
+	}
+
+	for _, test := range tests {
+		if output := construct(test.caps, test.tables, test.n); output != test.expected {
+			t.Errorf("Test Failed: {%s} inputted, {%s} expected, recieved: {%s}\n",
+				fmt.Sprint(test.caps, test.tables, test.n), test.expected, output)
+		}
+	}
+
+}
